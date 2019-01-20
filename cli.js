de4cb8f1aa91e6df2b22060bfb1ccd12b7fe4420
Alexander Alemayhu <alexander@alemayhu.com>
2019-01-20 13:13:34 +0100

    simplify output and use chalk
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- cli.js
+++ cli.js
@@ -1,9 +1,9 @@
 #!/bin/env node
 
 const project = require("./lib/project_setup");
-
 const version = require("./package.json").version;
 const cli = require("cac")();
+const chalk = require("chalk");
 
 cli.usage("path-to-new-project");
 cli.version(version);
@@ -12,17 +12,13 @@ cli.help();
 const parsed = cli.parse();
 
 const path = parsed.args[0];
-
+// TODO: Should we ask user questions?
 if (path) {
   project
     .InitializeRepository(path)
     .then(project.CreateStructure(path))
     .then(() => {
-      console.log("Project ready for you at", path);
-      console.log("Happy hacking!");
-      // TODO: commit it all!
-
-      // Should we ask user questions?
+      console.log("Project ready for you at", chalk.green.underline(path));
     });
 } else {
   cli.outputHelp();
